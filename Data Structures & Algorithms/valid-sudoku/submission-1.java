f111b0b6eb56cddebca821b95e350d00afc3edde
Pradeep Kumar S <96918763+Pradeep-Kumar-S-000@users.noreply.github.com>
2026-07-21 22:08:04 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] columns = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+
+        for(int r = 0; r < 9; r++) {
+            for(int c = 0; c < 9; c++) {
+                char ch = board[r][c];
+                if(ch == '.') continue;
+
+                int num = ch - '1';
+                int box = (r/3)*3 + (c/3);
+
+                if(rows[r][num] || columns[c][num] || boxes[box][num])
+                    return false;
+
+                rows[r][num] = true;
+                columns[c][num] = true;
+                boxes[box][num] = true;
+            }
+        }
+        return true;
+    }
+}
