fcabe6352d2b47e9ea758dbf8324d4d0f466b0b9
Pradeep Kumar S <96918763+Pradeep-Kumar-S-000@users.noreply.github.com>
2026-07-24 19:52:03 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.java
@@ -0,0 +1,19 @@
+class KthLargest {
+    public int k;
+    public List<Integer> numsList;
+    
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        // this.numsList = new ArrayList<Integer>(Arrays.asList(nums));
+        this.numsList = new ArrayList<Integer>();
+        for(int num : nums) numsList.add(num);
+    }
+    
+    public int add(int val) {
+        numsList.add(val);
+        return numsList.stream().sorted(Comparator.reverseOrder())
+            .skip(k-1)
+            .findFirst()
+            .orElse(0);
+    }
+}
